e0276a91530a21401c3da16d7ad1c3b5b4f26aeb
ShaneDalyy <123744783+ShaneDalyy@users.noreply.github.com>
2023-02-15 13:43:13 +0000

    Add files via upload

---
+++ artihmetic.js
@@ -0,0 +1,132 @@
+function incrementButton1()
+{
+    var element = document.getElementById('IncrementText1');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText1').innerHTML=value;
+
+}
+function decrementButton1()
+{
+    var element2 = document.getElementById('IncrementText1');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText1').innerHTML=value2;
+}
+function incrementButton2()
+{
+    var element = document.getElementById('IncrementText2');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText2').innerHTML=value;
+
+}
+function decrementButton2()
+{
+    var element2 = document.getElementById('IncrementText2');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText2').innerHTML=value2;
+}
+function incrementButton3()
+{
+    var element = document.getElementById('IncrementText3');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText3').innerHTML=value;
+
+}
+function decrementButton3()
+{
+    var element2 = document.getElementById('IncrementText3');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText3').innerHTML=value2;
+}
+function incrementButton4()
+{
+    var element = document.getElementById('IncrementText4');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText4').innerHTML=value;
+
+}
+function decrementButton4()
+{
+    var element2 = document.getElementById('IncrementText4');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText4').innerHTML=value2;
+}
+function incrementButton5()
+{
+    var element = document.getElementById('IncrementText5');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText5').innerHTML=value;
+
+}
+function decrementButton5()
+{
+    var element2 = document.getElementById('IncrementText5');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText5').innerHTML=value2;
+}
+function incrementButton6()
+{
+    var element = document.getElementById('IncrementText6');
+    var value = element.innerHTML;
+    value++;
+
+    console.log(value);
+    document.getElementById('IncrementText6').innerHTML=value;
+
+}
+function decrementButton6()
+{
+    var element2 = document.getElementById('IncrementText6');
+    var value2 = element2.innerHTML;
+    if(value2 >0)
+    {
+        value2--;
+    }
+
+    console.log(value2);
+    document.getElementById('IncrementText6').innerHTML=value2;
+}
